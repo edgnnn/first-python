facf842781ecd40f1e2aba03627dac0f2c265746
edgunnn <edigunawannnnn@gmail.com>
2023-01-08 07:08:25 +0700

    sequential joke beli telor

---
+++ main beneran.py
@@ -0,0 +1,13 @@
+"""
+semua sintaksis bahasa pemrograman terdiri dari :
+1.Sekuensial:  langkah berurutan
+2.Percabangan: langkah melompat jika kondisi terpenuhi
+3.Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+
+#sekuensial
+print ('Ibu berkata, "pergi ke toko"')
+print ('budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print ('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+print("Maka Budi berangkat ke toko")
+print("Dan mulai berbelanja")
